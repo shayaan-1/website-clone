ca7a7c23894f372cc7612cd6df2ed6928d208aef
Shayaan <shayanbutt561@gmail.com>
2024-03-18 05:13:50 +0500

    Initial commit of html css js files

---
+++ index.js
@@ -0,0 +1,31 @@
+document.getElementById("course-1").addEventListener("click", function() {
+    window.open("https://www.youtube.com/watch?v=jMKRUo4wVKA&t=3s", "_blank");
+});
+
+document.getElementById("course-2").addEventListener("click", function() {
+    window.open("https://www.youtube.com/watch?v=H4YK_7MAckk", "_blank");
+});
+
+document.getElementById("course-3").addEventListener("click", function() {
+    window.open("https://www.youtube.com/watch?v=jFo_gDOOusk", "_blank");
+});
+
+document.getElementById("course-4").addEventListener("click", function() {
+    window.open("https://www.youtube.com/watch?v=jFo_gDOOusk", "_blank");
+});
+
+document.getElementById("course-5").addEventListener("click", function() {
+    window.open("https://www.youtube.com/watch?v=fo7bD-NgA4Y", "_blank");
+});
+
+document.getElementById("course-6").addEventListener("click", function() {
+    window.open("https://www.youtube.com/watch?v=9PxhCekQYNI", "_blank");
+});
+
+document.getElementById("course-7").addEventListener("click", function() {
+    window.open("https://www.youtube.com/watch?v=Sh4n0uk-NHk", "_blank");
+});
+
+document.getElementById("course-8").addEventListener("click", function() {
+    window.open("https://www.youtube.com/watch?v=4fkqXdcTmGk", "_blank");
+});
